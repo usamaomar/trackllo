bcd5fef278825d65cf304d02b495fc257bee3041
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-12-02 11:53:08 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/tracking_page/tracking_page_widget.dart
+++ lib/pages/tracking_page/tracking_page_widget.dart
@@ -1,3 +1,4 @@
+import '/flutter_flow/flutter_flow_google_map.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
@@ -64,6 +65,7 @@ class _TrackingPageWidgetState extends State<TrackingPageWidget> {
           : FocusScope.of(context).unfocus(),
       child: Scaffold(
         key: scaffoldKey,
+        resizeToAvoidBottomInset: false,
         backgroundColor: const Color(0x0057636C),
         drawer: SizedBox(
           width: 300.0,
@@ -219,6 +221,29 @@ class _TrackingPageWidgetState extends State<TrackingPageWidget> {
                     ],
                   ),
                 ],
+              ),
+              Container(
+                width: 0.0,
+                height: 0.0,
+                decoration: const BoxDecoration(),
+                child: FlutterFlowGoogleMap(
+                  controller: _model.googleMapsController,
+                  onCameraIdle: (latLng) => _model.googleMapsCenter = latLng,
+                  initialLocation: _model.googleMapsCenter ??=
+                      const LatLng(13.106061, -59.613158),
+                  markerColor: GoogleMarkerColor.violet,
+                  mapType: MapType.normal,
+                  style: GoogleMapStyle.standard,
+                  initialZoom: 14.0,
+                  allowInteraction: true,
+                  allowZoom: true,
+                  showZoomControls: true,
+                  showLocation: true,
+                  showCompass: false,
+                  showMapToolbar: false,
+                  showTraffic: false,
+                  centerMapOnMarkerTap: true,
+                ),
               ),
             ],
           ),
