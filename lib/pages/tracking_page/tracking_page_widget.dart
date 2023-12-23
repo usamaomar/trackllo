babe5742eb130f9f19254c49004f1f382c75bef5
usamaomar <usamaomarsoftware@gmail.com>
2023-12-23 15:26:43 +0300

    changes

--- lib/pages/tracking_page/tracking_page_widget.dart
+++ lib/pages/tracking_page/tracking_page_widget.dart
@@ -52,9 +52,7 @@ class _TrackingPageWidgetState extends State<TrackingPageWidget> {
         ),
       );
     }
-
     context.watch<FFAppState>();
-
     return GestureDetector(
       onTap: () => _model.unfocusNode.canRequestFocus
           ? FocusScope.of(context).requestFocus(_model.unfocusNode)
